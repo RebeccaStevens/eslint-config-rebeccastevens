895412001f6eeab302b3308cbfd58bed8cba3366
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2019-11-21 03:07:13 +1300

    build(rollup): better local file detection

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,11 @@
  */
 // @ts-check
 
+import {
+  isAbsolute as isAbsolutePath,
+  join as joinPaths,
+  normalize as normalizePath
+} from 'path';
 import rollupPluginCommonjs from 'rollup-plugin-commonjs';
 import rollupPluginNodeResolve from 'rollup-plugin-node-resolve';
 import rollupPluginTypescript from 'rollup-plugin-typescript2';
@@ -10,9 +15,18 @@ import rollupPluginTypescript from 'rollup-plugin-typescript2';
 const common = {
   input: 'src/index.ts',
 
-  external: (id) =>
-    // Not a Local File?
-    !(id.startsWith('.') || id.startsWith('/')),
+  external: (id) => {
+    const localPaths = ['.', process.cwd()];
+    const excludedPaths = ['node_modules'];
+    const normalId = isAbsolutePath(id) ? normalizePath(id) : id;
+
+    return !localPaths.some((localPath) =>
+      // Local file?
+      normalId.startsWith(localPath) &&
+      // Not excluded?
+      !excludedPaths.some((excludePath) =>
+        normalId.startsWith(joinPaths(localPath, excludePath))));
+  },
 
   treeshake: {
     annotations: true,
