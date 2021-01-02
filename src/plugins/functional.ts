c5d6ae761035e5d036d97b38b19e30593481ee7c
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-01-03 00:45:38 +1300

    fix: opt-in to functional/stylitic rules

--- src/plugins/functional.ts
+++ src/plugins/functional.ts
@@ -3,7 +3,11 @@ import type { Linter } from "eslint";
 export const settings: Linter.Config = {
   plugins: ["functional"],
 
-  extends: ["plugin:functional/external-recommended", "plugin:functional/recommended"],
+  extends: [
+    "plugin:functional/external-recommended",
+    "plugin:functional/recommended",
+    "plugin:functional/stylitic",
+  ],
 
   rules: {
     "functional/prefer-readonly-type": [
