787f90e73cd6e7b057840d2edcb438de3a07742e
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-07-21 23:02:06 +1200

    fix(non-nullable-type-assertion-style): add rule

--- src/plugins/typescript.ts
+++ src/plugins/typescript.ts
@@ -112,6 +112,7 @@ export const settings: Linter.Config = {
         typedefs: true,
       },
     ],
+    "@typescript-eslint/non-nullable-type-assertion-style": "error",
     "@typescript-eslint/restrict-template-expressions": [
       "error",
       {
