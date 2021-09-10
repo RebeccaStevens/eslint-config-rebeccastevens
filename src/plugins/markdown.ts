ba3d4ec6c03d69a7d1b52b9fe1aa27118268dd33
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-09-10 14:34:19 +1200

    feat: update markdown config

--- src/plugins/markdown.ts
+++ src/plugins/markdown.ts
@@ -2,4 +2,20 @@ import type { Linter } from "eslint";
 
 export const settings: Linter.Config = {
   plugins: ["markdown"],
+
+  extends: ["plugin:markdown/recommended"],
+
+  overrides: [
+    {
+      files: ["**/*.md"],
+      processor: "markdown/markdown",
+    },
+    {
+      files: ["**/*.md/**"],
+      rules: {
+        "no-console": "off",
+        "prettier/prettier": "off",
+      },
+    },
+  ],
 };
