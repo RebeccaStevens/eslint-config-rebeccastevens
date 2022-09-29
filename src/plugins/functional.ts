d8d8420774fbb25ce1fa4d42f4d79e2b4865e126
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-09-29 20:43:39 +1300

    fix: update functional rules

--- src/plugins/functional.ts
+++ src/plugins/functional.ts
@@ -3,6 +3,8 @@ import type { Linter } from "eslint";
 export const settings: Linter.Config = {
   plugins: ["functional"],
 
+  extends: ["plugin:functional/recommended"],
+
   rules: {
     "functional/no-expression-statement": "error",
     "functional/immutable-data": [
@@ -13,12 +15,6 @@ export const settings: Linter.Config = {
         ignoreImmediateMutation: true,
       },
     ],
-    "functional/no-conditional-statement": [
-      "error",
-      {
-        allowReturningBranches: true,
-      },
-    ],
     "functional/no-let": [
       "error",
       {
@@ -26,24 +22,5 @@ export const settings: Linter.Config = {
         ignorePattern: ["^mutable", "^m_"],
       },
     ],
-    "functional/no-loop-statement": "error",
-    "functional/no-mixed-type": "error",
-    "functional/no-throw-statement": [
-      "error",
-      {
-        allowInAsyncFunctions: true,
-      },
-    ],
-    "functional/no-return-void": "error",
   },
-
-  overrides: [
-    {
-      files: ["**/*.cjs"],
-      rules: {
-        "functional/immutable-data": "off",
-        "functional/no-expression-statement": "off",
-      },
-    },
-  ],
 };
