2fce7d05d3aceee5a7eeba5fe198aede1e836df7
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-03-11 20:45:12 +1300

    fix: update jsdoc rules

--- src/plugins/jsdoc.ts
+++ src/plugins/jsdoc.ts
@@ -4,16 +4,31 @@ export const settings: Linter.Config = {
   plugins: ["jsdoc"],
 
   rules: {
+    "jsdoc/check-access": "warn",
     "jsdoc/check-alignment": "warn",
+    // waiting on https://github.com/eslint/eslint/issues/14745
+    // "jsdoc/check-examples": "warn",
     "jsdoc/check-indentation": "warn",
+    "jsdoc/check-line-alignment": "warn",
     "jsdoc/check-param-names": "warn",
+    "jsdoc/check-property-names": "warn",
     "jsdoc/check-tag-names": "warn",
+    "jsdoc/check-types": "warn",
+    "jsdoc/check-values": "warn",
+    "jsdoc/empty-tags": "warn",
     "jsdoc/implements-on-classes": "warn",
+    "jsdoc/multiline-blocks": "warn",
     "jsdoc/newline-after-description": "warn",
+    "jsdoc/no-bad-blocks": "warn",
+    "jsdoc/no-defaults": "warn",
+    "jsdoc/no-multi-asterisks": "warn",
+    // For TypeScript
     "jsdoc/no-types": "warn",
+    "jsdoc/no-undefined-types": "warn",
+    "jsdoc/require-asterisk-prefix": "warn",
     "jsdoc/require-description": "warn",
     // Rule is too strict.
-    "jsdoc/require-description-complete-sentence": "off",
+    // "jsdoc/require-description-complete-sentence": "off",
     "jsdoc/require-hyphen-before-param-description": "warn",
     "jsdoc/require-jsdoc": [
       "warn",
@@ -34,7 +49,17 @@ export const settings: Linter.Config = {
     ],
     "jsdoc/require-param-description": "warn",
     "jsdoc/require-param-name": "warn",
+    "jsdoc/require-property-description": "warn",
+    "jsdoc/require-property-name": "warn",
     "jsdoc/require-returns-check": "warn",
     "jsdoc/require-returns-description": "warn",
+    "jsdoc/require-yields": "warn",
+    "jsdoc/require-yields-check": "warn",
+    "jsdoc/tag-lines": [
+      "warn",
+      "never",
+      { tags: { example: { lines: "always" } } },
+    ],
+    "jsdoc/valid-types": "warn",
   },
 };
