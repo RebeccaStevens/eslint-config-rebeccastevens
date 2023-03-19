9203df49d6fbd0a8459cfe8e0e4a5c2a455b6a66
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-03-19 17:14:08 +1300

    fix: update test config

--- src/configs/test.ts
+++ src/configs/test.ts
@@ -4,13 +4,27 @@ const baseConfig: Linter.Config = {
   extends: ["@rebeccastevens/eslint-config/script"],
 
   rules: {
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "eslint-comments/disable-enable-pair": "off",
     "functional/functional-parameters": "off",
     "functional/immutable-data": "warn",
     "functional/no-conditional-statements": "warn",
     "functional/no-expression-statements": "off",
     "functional/no-loop-statements": "warn",
     "functional/no-throw-statements": "off",
+    "jsdoc/require-jsdoc": "off",
+    "lines-between-class-members": "off",
+    "max-classes-per-file": "off",
     "node/no-sync": "off",
+    "sonarjs/no-duplicate-string": "off",
+    "sonarjs/no-identical-functions": "off",
+    "unicorn/consistent-function-scoping": "off",
   },
 };
 
