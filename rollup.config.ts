69d41ba83be22f05424413d3f3259fbe04660603
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2025-03-17 13:53:17 +1300

    fix: build

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,18 @@
+import assert from "node:assert";
+
 import rollupPluginTypescript from "@rollup/plugin-typescript";
 import type { RollupOptions } from "rollup";
 import rollupPluginDeassert from "rollup-plugin-deassert";
 import generateDtsBundle from "rollup-plugin-dts-bundle-generator-2";
+import {
+  type Node,
+  type SourceFile,
+  SyntaxKind,
+  isCallExpression,
+  isIdentifier,
+  visitEachChild,
+  visitNode,
+} from "typescript";
 
 import pkg from "./package.json" with { type: "json" };
 
@@ -35,6 +46,32 @@ export default {
   plugins: [
     rollupPluginTypescript({
       tsconfig: "src/tsconfig.build.json",
+      transformers: {
+        after: [
+          /**
+           * Strip out TS relative import path rewrite helper from dynamic import() calls
+           *
+           * Required due to
+           * https://github.com/rollup/plugins/issues/1820
+           */
+          (context) => (source) => {
+            function visitor(node: Node) {
+              if (
+                isCallExpression(node) &&
+                isIdentifier(node.expression) &&
+                node.expression.text === "__rewriteRelativeImportExtension" &&
+                node.arguments.length === 1
+              ) {
+                return node.arguments[0]!;
+              }
+              return visitEachChild(node, visitor, context);
+            }
+            const result = visitNode(source, visitor);
+            assert(result.kind === SyntaxKind.SourceFile);
+            return result as SourceFile;
+          },
+        ],
+      },
     }),
     rollupPluginDeassert({
       include: ["**/*.{js,ts}"],
